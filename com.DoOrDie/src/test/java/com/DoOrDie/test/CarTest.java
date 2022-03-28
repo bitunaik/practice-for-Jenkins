b6f23b0ea4e01a1f7fc6bf556b8f2f4b07c83232
Omprakash <Omprakash@Omprakash>
2022-03-28 16:00:09 +0530

    commited

---
+++ com.DoOrDie/src/test/java/com/DoOrDie/test/CarTest.java
@@ -0,0 +1,10 @@
+package com.DoOrDie.test;
+
+import org.testng.annotations.Test;
+
+public class CarTest {
+@Test
+public void car() {
+	System.out.println("car");
+}
+}
---
+++ com.DoOrDie/src/test/java/com/DoOrDie/test/MavenTest.java
@@ -0,0 +1,10 @@
+package com.DoOrDie.test;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+@Test
+public void mven() {
+	System.out.println("group");
+}
+}
